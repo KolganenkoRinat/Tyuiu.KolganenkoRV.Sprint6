e334543396ae9c3ce24bf2f26f8457831786d8e2
Tyuiu.KolganenkoRV@outlook.com <kolganenko.rinat@mail.ru>
2025-12-01 01:08:50 +0500

    Task1

--- Tyuiu.KolganenkoRV.Sprint6.Task1.V13.Lib/DataService.cs
+++ Tyuiu.KolganenkoRV.Sprint6.Task1.V13.Lib/DataService.cs
@@ -12,7 +12,7 @@ namespace Tyuiu.KolganenkoRV.Sprint6.Task1.V13.Lib
             double s;
             for (int x = startValue; x <= stopValue; x++)
             {
-                s = ((3 * Math.Cos(x)) / (4 * x - 0.5)) + Math.Sin(x) - 5 * x - 2; 
+                s = ((3 * Math.Cos(x)) / (4 * x - 0.5)) + Math.Sin(x) - 5 * x - 2;
                 valueArray[count] = Math.Round(s, 2);
                 count++;
             }

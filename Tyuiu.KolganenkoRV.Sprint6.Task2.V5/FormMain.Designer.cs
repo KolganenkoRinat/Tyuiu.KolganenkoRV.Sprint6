b75ac9b3f280ff5644407688837062affe9b8adb
Tyuiu.KolganenkoRV@outlook.com <kolganenko.rinat@mail.ru>
2025-12-03 12:55:33 +0500

    Task2

--- Tyuiu.KolganenkoRV.Sprint6.Task2.V5/FormMain.Designer.cs
+++ Tyuiu.KolganenkoRV.Sprint6.Task2.V5/FormMain.Designer.cs
@@ -31,23 +31,32 @@
             textBoxStart_KRV = new TextBox();
             textBoxEnd_KRV = new TextBox();
             dataGridViewFunction_KRV = new DataGridView();
-            buttoninfo_KRV = new Button();
-            buttonStart_KRV = new Button();
             Column1 = new DataGridViewTextBoxColumn();
             Column2 = new DataGridViewTextBoxColumn();
+            buttoninfo_KRV = new Button();
+            buttonStart_KRV = new Button();
+            groupBoxTask_KRV = new GroupBox();
+            groupBoxInPut_KRV = new GroupBox();
+            groupBoxOutPut_KRV = new GroupBox();
+            textBoxTask_KRV = new TextBox();
+            textBoxStr_KRV = new TextBox();
+            textBoxStp_KRV = new TextBox();
             ((System.ComponentModel.ISupportInitialize)dataGridViewFunction_KRV).BeginInit();
+            groupBoxTask_KRV.SuspendLayout();
+            groupBoxInPut_KRV.SuspendLayout();
+            groupBoxOutPut_KRV.SuspendLayout();
             SuspendLayout();
             // 
             // textBoxStart_KRV
             // 
-            textBoxStart_KRV.Location = new Point(50, 393);
+            textBoxStart_KRV.Location = new Point(6, 60);
             textBoxStart_KRV.Name = "textBoxStart_KRV";
             textBoxStart_KRV.Size = new Size(133, 23);
             textBoxStart_KRV.TabIndex = 0;
             // 
             // textBoxEnd_KRV
             // 
-            textBoxEnd_KRV.Location = new Point(208, 393);
+            textBoxEnd_KRV.Location = new Point(188, 60);
             textBoxEnd_KRV.Name = "textBoxEnd_KRV";
             textBoxEnd_KRV.Size = new Size(133, 23);
             textBoxEnd_KRV.TabIndex = 1;
@@ -56,31 +65,11 @@
             // 
             dataGridViewFunction_KRV.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             dataGridViewFunction_KRV.Columns.AddRange(new DataGridViewColumn[] { Column1, Column2 });
-            dataGridViewFunction_KRV.Location = new Point(378, 43);
+            dataGridViewFunction_KRV.Location = new Point(9, 26);
             dataGridViewFunction_KRV.Name = "dataGridViewFunction_KRV";
             dataGridViewFunction_KRV.RowHeadersVisible = false;
             dataGridViewFunction_KRV.Size = new Size(103, 276);
             dataGridViewFunction_KRV.TabIndex = 2;
-            // 
-            // buttoninfo_KRV
-            // 
-            buttoninfo_KRV.Location = new Point(50, 43);
-            buttoninfo_KRV.Name = "buttoninfo_KRV";
-            buttoninfo_KRV.Size = new Size(291, 70);
-            buttoninfo_KRV.TabIndex = 3;
-            buttoninfo_KRV.Text = "хуй";
-            buttoninfo_KRV.UseVisualStyleBackColor = true;
-            buttoninfo_KRV.Click += buttoninfo_KRV_Click;
-            // 
-            // buttonStart_KRV
-            // 
-            buttonStart_KRV.Location = new Point(50, 125);
-            buttonStart_KRV.Name = "buttonStart_KRV";
-            buttonStart_KRV.Size = new Size(291, 194);
-            buttonStart_KRV.TabIndex = 4;
-            buttonStart_KRV.Text = "пизда";
-            buttonStart_KRV.UseVisualStyleBackColor = true;
-            buttonStart_KRV.Click += buttonStart_KRV_Click;
             // 
             // Column1
             // 
@@ -94,21 +83,107 @@
             Column2.Name = "Column2";
             Column2.Width = 50;
             // 
+            // buttoninfo_KRV
+            // 
+            buttoninfo_KRV.Location = new Point(367, 372);
+            buttoninfo_KRV.Name = "buttoninfo_KRV";
+            buttoninfo_KRV.Size = new Size(73, 63);
+            buttoninfo_KRV.TabIndex = 3;
+            buttoninfo_KRV.Text = "Справка";
+            buttoninfo_KRV.UseVisualStyleBackColor = true;
+            buttoninfo_KRV.Click += buttoninfo_KRV_Click;
+            // 
+            // buttonStart_KRV
+            // 
+            buttonStart_KRV.Location = new Point(455, 372);
+            buttonStart_KRV.Name = "buttonStart_KRV";
+            buttonStart_KRV.Size = new Size(133, 63);
+            buttonStart_KRV.TabIndex = 4;
+            buttonStart_KRV.Text = "Выполнить";
+            buttonStart_KRV.UseVisualStyleBackColor = true;
+            buttonStart_KRV.Click += buttonStart_KRV_Click;
+            // 
+            // groupBoxTask_KRV
+            // 
+            groupBoxTask_KRV.Controls.Add(textBoxTask_KRV);
+            groupBoxTask_KRV.Location = new Point(5, 8);
+            groupBoxTask_KRV.Name = "groupBoxTask_KRV";
+            groupBoxTask_KRV.Size = new Size(492, 329);
+            groupBoxTask_KRV.TabIndex = 5;
+            groupBoxTask_KRV.TabStop = false;
+            groupBoxTask_KRV.Text = "Условие";
+            // 
+            // groupBoxInPut_KRV
+            // 
+            groupBoxInPut_KRV.Controls.Add(textBoxStp_KRV);
+            groupBoxInPut_KRV.Controls.Add(textBoxStr_KRV);
+            groupBoxInPut_KRV.Controls.Add(textBoxEnd_KRV);
+            groupBoxInPut_KRV.Controls.Add(textBoxStart_KRV);
+            groupBoxInPut_KRV.Location = new Point(8, 352);
+            groupBoxInPut_KRV.Name = "groupBoxInPut_KRV";
+            groupBoxInPut_KRV.Size = new Size(353, 90);
+            groupBoxInPut_KRV.TabIndex = 6;
+            groupBoxInPut_KRV.TabStop = false;
+            groupBoxInPut_KRV.Text = "Ввод данных:";
+            // 
+            // groupBoxOutPut_KRV
+            // 
+            groupBoxOutPut_KRV.Controls.Add(dataGridViewFunction_KRV);
+            groupBoxOutPut_KRV.Location = new Point(524, 8);
+            groupBoxOutPut_KRV.Name = "groupBoxOutPut_KRV";
+            groupBoxOutPut_KRV.Size = new Size(597, 329);
+            groupBoxOutPut_KRV.TabIndex = 7;
+            groupBoxOutPut_KRV.TabStop = false;
+            groupBoxOutPut_KRV.Text = "Вывод данных:";
+            // 
+            // textBoxTask_KRV
+            // 
+            textBoxTask_KRV.Location = new Point(5, 23);
+            textBoxTask_KRV.Multiline = true;
+            textBoxTask_KRV.Name = "textBoxTask_KRV";
+            textBoxTask_KRV.ReadOnly = true;
+            textBoxTask_KRV.Size = new Size(472, 66);
+            textBoxTask_KRV.TabIndex = 0;
+            textBoxTask_KRV.Text = "Протабулировать функцию на заданном диапазоне.  Результат вывести в DataGridView и построить график функции.";
+            // 
+            // textBoxStr_KRV
+            // 
+            textBoxStr_KRV.Location = new Point(6, 22);
+            textBoxStr_KRV.Name = "textBoxStr_KRV";
+            textBoxStr_KRV.ReadOnly = true;
+            textBoxStr_KRV.Size = new Size(133, 23);
+            textBoxStr_KRV.TabIndex = 2;
+            textBoxStr_KRV.Text = "Старт шага:";
+            // 
+            // textBoxStp_KRV
+            // 
+            textBoxStp_KRV.Location = new Point(188, 22);
+            textBoxStp_KRV.Multiline = true;
+            textBoxStp_KRV.Name = "textBoxStp_KRV";
+            textBoxStp_KRV.ReadOnly = true;
+            textBoxStp_KRV.Size = new Size(133, 23);
+            textBoxStp_KRV.TabIndex = 3;
+            textBoxStp_KRV.Text = "Конец шага:";
+            // 
             // FormMain
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(1128, 450);
+            Controls.Add(groupBoxOutPut_KRV);
+            Controls.Add(groupBoxInPut_KRV);
+            Controls.Add(groupBoxTask_KRV);
             Controls.Add(buttonStart_KRV);
             Controls.Add(buttoninfo_KRV);
-            Controls.Add(dataGridViewFunction_KRV);
-            Controls.Add(textBoxEnd_KRV);
-            Controls.Add(textBoxStart_KRV);
             Name = "FormMain";
             Text = "Спринт 6 | Таск 2 | Колганенко Р.В.";
             ((System.ComponentModel.ISupportInitialize)dataGridViewFunction_KRV).EndInit();
+            groupBoxTask_KRV.ResumeLayout(false);
+            groupBoxTask_KRV.PerformLayout();
+            groupBoxInPut_KRV.ResumeLayout(false);
+            groupBoxInPut_KRV.PerformLayout();
+            groupBoxOutPut_KRV.ResumeLayout(false);
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
@@ -120,5 +195,11 @@
         private Button buttonStart_KRV;
         private DataGridViewTextBoxColumn Column1;
         private DataGridViewTextBoxColumn Column2;
+        private GroupBox groupBoxTask_KRV;
+        private GroupBox groupBoxInPut_KRV;
+        private GroupBox groupBoxOutPut_KRV;
+        private TextBox textBoxTask_KRV;
+        private TextBox textBoxStp_KRV;
+        private TextBox textBoxStr_KRV;
     }
 }
